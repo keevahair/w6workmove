4783b607f399324a388caddf184e26c60cdc914a
kazmina <kazminagirl@hotmail.com>
2024-11-24 16:09:34 -0500

    change

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "mon1stbucket"
+    bucket = "mon1sjiurtuytrtbucket"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "statefilelock"
+    dynamodb_table = "statenjhiiyuuigitfilelock"
     encrypt = true
     
   }
